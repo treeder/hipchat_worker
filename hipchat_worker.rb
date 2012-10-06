b1ce43006dbf2b308ceffb0e38b8266e6e183b90
Travis <treeder@gmail.com>
2012-10-06 12:56:52 -0700

    Added from default.

--- hipchat_worker.rb
+++ hipchat_worker.rb
@@ -3,7 +3,7 @@ require 'hipchat-api'
 hipchat = HipChat::API.new(params[:hipchat][:api_key])
 
 r = hipchat.rooms_message(params[:hipchat][:room],
-                          '',
+                          params[:from] || "HipChatWorker",
                           params[:body],
                           true)
 
